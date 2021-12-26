740bf3688ecf3a97d291485c3df959c0b9a7febb
hadashirt <hadashirt12@gmail.com>
2021-12-26 11:17:13 -0500

    first draft

--- my_mem.c
+++ my_mem.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-void mem_init(unsigned char *my_memory, unsigned int my_mem_size)
-{
-    /*How do i initialize ? What am I initializing? */
-    /*need to define a maz number for memory */
-    char free_list[my_mem_size];
-    int free = 1;
-    char nextt = NULL;
+
+void mem_init(unsigned char *my_memory, unsigned int my_mem_size){
+/*How do i initialize ? What am I initializing? */
+/*need to define a maz number for memory */
+char free_list[my_mem_size];
+int free=1;
+char nextt= NULL;
+
+
 }
-void *my_malloc(unsigned size)
-{
-    /*test if the memory we are accessing is past what we have*/
+void *my_malloc(unsigned size){
+/*test if the memory we are accessing is past what we have and if not add*/
+/* are we supposed to make a heap? */
     void *mem;
 
-    if (sizeof my_memory - nextt < size)
+    if(sizeof my_memory - nextt < size)
         return NULL;
 
     mem = &my_memory[nextt];
@@ -21,4 +23,5 @@ void *my_malloc(unsigned size)
     return mem;
 }
 
-void my_free(void *mem_pointer) void mem_get_stats(mem_stats_ptr mem_stats_ptr) * /
+void my_free(void *mem_pointer)
+void mem_get_stats(mem_stats_ptr mem_stats_ptr)
