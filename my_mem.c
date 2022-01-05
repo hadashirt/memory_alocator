5c88164a669591dbfebd4e5f3f30e870249c015b
hadashirt <hadashirt12@gmail.com>
2022-01-04 23:55:56 -0500

    updates

--- my_mem.c
+++ my_mem.c
@@ -22,14 +22,14 @@ mem_stats_struct mem_stats_s;
 
 /*initialize the linked list */
 // A linked list node
-typedef struct Node {
-  int data;
+struct Node {
+  int size;
+  unsigned char *mem_add_head;
   struct Node *next;
   struct Node *prev;
-  unsigned char *mem_add_head;
-} free_list, used_list;
-free_list *head_free = NULL;
-used_list *head_used = NULL;
+};
+struct Node *head_free = NULL;
+struct Node *head_used = NULL;
 
 void mem_init(unsigned char *my_memory, unsigned int my_mem_size) {
   /*pointer pointing to the struct*/
@@ -42,13 +42,17 @@ void mem_init(unsigned char *my_memory, unsigned int my_mem_size) {
   mem_stats_s.largest_block_used = 0;
 
   /* pointer to the begining of the linked list free, the mem adress of it */
-  head_free->mem_add_head = my_memory + sizeof(struct Node);
-  head_free->data = my_mem_size;
+  head_free = (struct Node *)malloc(sizeof(struct Node));
+  head_free->mem_add_head = my_memory;
+  head_free->size = my_mem_size;
   head_free->next = NULL;
+  head_free->prev = NULL;
   /*initialize the used list as empty*/
+  head_used = (struct Node *)malloc(sizeof(struct Node));
   head_used->mem_add_head = NULL;
-  head_used->data = 0;
+  head_used->size = 0;
   head_used->next = NULL;
+  head_used->prev = NULL;
 
   cur_mem_used = sizeof(struct Node);
   total_mem_size = my_mem_size;
@@ -62,16 +66,19 @@ void *my_malloc(unsigned size) {
     exit(-1);
   }
   /*if there is enough space continue:*/
-  while (head_free->next != NULL) {
-    if (head_free->data >= size) {
+  struct Node *ptr = head_free;
+  // temp pointer set to head , as long as ptr is not null ,set the ptr to next
+  while (ptr != NULL) {
+    if (head_free->size >= size) {
       head_used->mem_add_head = head_free->mem_add_head;
-      head_free->mem_add_head =
-          head_free->mem_add_head + size + sizeof(struct Node);
-      head_used->data = size + sizeof(struct Node);
+      head_free->mem_add_head = head_free->mem_add_head - size;
+      head_used->size = size;
+      break;
     }
+    ptr = ptr->next;
   }
   /*increment cur mem by size and size of header*/
-  cur_mem_used += size + sizeof(struct Node);
+  cur_mem_used += size;
   /*increment number of blocks used*/
   mem_stats_s.num_blocks_used += 1;
   /* if all the space is used set the num of free blocks to 0*/
@@ -79,7 +86,7 @@ void *my_malloc(unsigned size) {
     mem_stats_s.num_blocks_free = 0;
   }
   /* change the smallest free block to the new size of the free block*/
-  mem_stats_s.smallest_block_free = total_mem_size - cur_mem_used - sizeof(int);
+  mem_stats_s.smallest_block_free = total_mem_size - cur_mem_used;
   /*check if this block is the smallest*/
   if (mem_stats_s.smallest_block_used == 0) {
     mem_stats_s.smallest_block_used = size;
@@ -93,7 +100,7 @@ void *my_malloc(unsigned size) {
     prevSmallestHeader = size;
   }
   /* change the largest free block to the new size of the free block*/
-  mem_stats_s.largest_block_free = total_mem_size - cur_mem_used - sizeof(int);
+  mem_stats_s.largest_block_free = total_mem_size - cur_mem_used;
   /*check if this block is the largest*/
   if ((size > mem_stats_s.largest_block_used) && (size > prevBiggestHeader)) {
     prevBiggestHeader = mem_stats_s.largest_block_used;
@@ -102,34 +109,40 @@ void *my_malloc(unsigned size) {
              (size > prevBiggestHeader)) {
     prevBiggestHeader = size;
   }
-  /*return pointer to the size of that insert*/
+  /*return pointer to the memory location of the insert*/
   return head_used->mem_add_head;
 };
+
 void my_free(void *mem_pointer) {
-  /*pass a pointer that is pointing to memory where the memory is , the header
-  should show you how big the data is in the part of the stack, move the offset
-  pointer back depending on size of the variable*/
-  /*get the size of the space that is being freed*/
-
-  int free_space = *(int *)mem_pointer;
-
-  /* move offset back the amount of free space*/
-  offset -= free_space + sizeof(int);
+  /*find the node that  is being removed and then remove it */
+  struct Node *ptr = head_used;
+  while (ptr != NULL) {
+    if ((head_used->mem_add_head = mem_pointer)) {
+      printf("%d\n", head_used->size);
+      head_free->mem_add_head = mem_pointer;
+      head_used->mem_add_head = mem_pointer -= head_used->size;
+      break;
+    }
+    head_used = head_used->next;
+    ptr = ptr->next;
+  }
+  // set the size of the space being freed
+  int free_space = head_used->size;
+  int free = free_space;
 
   /*update the cur mem_used*/
-
-  cur_mem_used -= free_space + sizeof(int);
+  cur_mem_used -= free_space;
 
   mem_stats_s.num_blocks_used -= 1;
-  mem_stats_s.num_blocks_free = 1;
-  /*should I add in an error message here if you cant remove a variable */
+  mem_stats_s.num_blocks_free += 1;
+  /*should I add in an error message here if you cant remove a variable*/
   mem_stats_s.smallest_block_free = total_mem_size - cur_mem_used;
-  /**/
+
   if (free_space == mem_stats_s.smallest_block_used) {
     mem_stats_s.smallest_block_used = prevSmallestHeader;
   }
   mem_stats_s.largest_block_free = total_mem_size - cur_mem_used;
-  /**/
+
   if (free_space == mem_stats_s.largest_block_used) {
     mem_stats_s.largest_block_used = prevBiggestHeader;
   }
--- test4.c
+++ test4.c
@@ -1,7 +1,7 @@
 #include "my_mem.c"
 #include <stdlib.h>
 
-/*remove out of order*/
+/*remove something that is not there */
 int main(int argc, char **argv) {
   unsigned int global_mem_size = 50 * 50;
   unsigned char *global_memory = malloc(global_mem_size);
@@ -24,7 +24,10 @@ int main(int argc, char **argv) {
   my_free(ptr_array[2]);
   print_stats("after free #1");
   my_free(ptr_array[0]);
-  print_stats("after free #1");
+  print_stats("after free #2");
   my_free(ptr_array[1]);
+  print_stats("after free #3");
+  // this was already removed
+  my_free(ptr_array[2]);
   print_stats("after free #1");
 }
